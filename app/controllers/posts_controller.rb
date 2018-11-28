9f9b0af8d03004ff84a9ced640a82549cc1d3f92
KerneyJ <gamerguy9801@gmail.com>
2018-11-28 16:46:32 -0600

    pages controller and add new post
    added a controller called pages and made a page that allows a person to submit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def index
     @title = 'Homepage'
   end
+
+  def new
+
+  end
+
+  def create
+    render plain: params[:post].inspect
+  end
+
 end
