828ce14c91ff14164ff345e10c3630b3cbddc97e
KerneyJ <gamerguy9801@gmail.com>
2018-11-28 17:29:45 -0600

    revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   root 'posts#index'
 
   resource :post
+=======
+>>>>>>> parent of ca7ffe7... Home page
 end
