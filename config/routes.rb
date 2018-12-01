859929ff623dea56d5916c91e9371900e393dcda
maroj44 <31449735+maroj44@users.noreply.github.com>
2018-12-01 15:50:30 -0600

    reset
    this patch resets the server to its default code on generation so it is easier to setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root 'posts#index'
-
-  resource :post
-=======
->>>>>>> parent of ca7ffe7... Home page
 end
